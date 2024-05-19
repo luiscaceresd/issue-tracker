c5934dd55e4b281d0c28cdc804af3e5e227b089e
luiscaceresd <luiscaceresd97@gmail.com>
2024-05-19 14:04:59 -0300

    init

---
+++ app/NavBar.tsx
@@ -0,0 +1,31 @@
+'use client';
+
+import Link from "next/link"
+import React from 'react'
+import { FaBug } from "react-icons/fa";
+import { usePathname } from "next/navigation";
+import classnames from "classnames";
+
+const NavBar = () => {
+  const currentPath = usePathname()
+
+  const links = [
+    {label: 'Dashboard', href: '/'},
+    {label: 'Issues', href: '/issues'}
+  ]
+
+  return (
+    <nav className="flex space-x-6 border-b mb-5 h-14 items-center px-5">
+      <Link href="/"><FaBug /></Link>
+      <ul className="flex space-x-6">
+        {links.map((link) => <li><Link key={link.href} href={link.href} className={classnames({
+          'text-zinc-50': link.href === currentPath,
+          'text-zinc-500': link.href !== currentPath,
+          'hover:text-white transition-colors': true
+        })}>{link.label}</Link></li>)}
+      </ul>
+    </nav>
+  )
+}
+
+export default NavBar
---
+++ prisma/migrations/20240519165125_fixing/migration.sql
@@ -0,0 +1,54 @@
+/*
+  Warnings:
+
+  - You are about to drop the `Category` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `Post` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `Profile` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `User` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `_CategoryToPost` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- CreateEnum
+CREATE TYPE "status" AS ENUM ('OPEN', 'IN_PROGRESS', 'CLOSED');
+
+-- DropForeignKey
+ALTER TABLE "Post" DROP CONSTRAINT "Post_authorId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "Profile" DROP CONSTRAINT "Profile_userId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "_CategoryToPost" DROP CONSTRAINT "_CategoryToPost_A_fkey";
+
+-- DropForeignKey
+ALTER TABLE "_CategoryToPost" DROP CONSTRAINT "_CategoryToPost_B_fkey";
+
+-- DropTable
+DROP TABLE "Category";
+
+-- DropTable
+DROP TABLE "Post";
+
+-- DropTable
+DROP TABLE "Profile";
+
+-- DropTable
+DROP TABLE "User";
+
+-- DropTable
+DROP TABLE "_CategoryToPost";
+
+-- DropEnum
+DROP TYPE "Role";
+
+-- CreateTable
+CREATE TABLE "Issue" (
+    "id" SERIAL NOT NULL,
+    "title" VARCHAR(255) NOT NULL,
+    "description" TEXT NOT NULL,
+    "status" "status" NOT NULL DEFAULT 'OPEN',
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Issue_pkey" PRIMARY KEY ("id")
+);
